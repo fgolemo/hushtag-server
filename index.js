fd11adf9dccba6f07be0d3bb8f43e85aac381c13
fgolemo <fgolemo@gmail.com>
2015-08-12 21:26:06 +0200

    testing heroku deploy

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var cool = require('cool-ascii-faces');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -9,12 +10,16 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.get('/', function (request, response) {
+    response.render('pages/index');
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.get('/cool', function (request, response) {
+    response.send(cool());
+});
+
+app.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
 });
 
 
