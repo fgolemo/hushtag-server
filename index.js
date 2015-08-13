97b2bed6f2c5313ee8a29a13c496fd6c094de753
fgolemo <fgolemo@gmail.com>
2015-08-13 20:10:30 +0200

    enabled CORS

--- index.js
+++ index.js
@@ -16,6 +16,11 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
 }));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
