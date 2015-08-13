aa5f87f559eca709d9100e0490e09115b4830c5c
fgolemo <fgolemo@gmail.com>
2015-08-13 19:47:55 +0200

    added statis content serving and moved redis db

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ client.auth(redisURL.auth.split(":")[1]);
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+app.use('/static', express.static(__dirname + '/public'));
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
