ac4c91374ef536a0ed319ce378b9753e581ebe4d
fgolemo <fgolemo@gmail.com>
2015-08-27 22:50:39 +0200

    added auth check when editing an object

--- modules/datainterface.js
+++ modules/datainterface.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var rest = require('./rest');
+var token = require('./token');
+var settings = require('./settings');
 
 module.exports = function(name, packer, unpacker) {
     'use strict';
@@ -9,7 +11,7 @@ module.exports = function(name, packer, unpacker) {
     app.get('/'+name+'/:id', getData);
     app.post('/'+name+'s', postData);
     app.put('/'+name+'/:id', putData);
-    app.delete('/'+name+'/:id', deleteData);
+    //app.delete('/'+name+'/:id', deleteData);
 
     function getAllData(req, res, next) {
         rest.getAll(name, res, next, unpacker);
@@ -21,19 +23,38 @@ module.exports = function(name, packer, unpacker) {
     }
 
     function postData(req, res, next) {
-        var obj = packer(req.body);
-        rest.createDetail(name, obj, res, next, unpacker);
+        var userToken = req.body.ut;
+        var obj = packer(req.body.obj);
+        _verifyUT(userToken, res, function() {
+            rest.createDetail(name, obj, res, next, unpacker);
+        });
     }
 
     function putData(req, res, next) {
         var id = req.params.id;
-        var obj = packer(req.body);
-        rest.updateDetail(name, id, obj, res, next, unpacker);
+        var userToken = req.body.ut;
+        var obj = packer(req.body.obj);
+        _verifyUT(userToken, res, function() {
+            rest.updateDetail(name, id, obj, res, next, unpacker);
+        });
+
     }
 
     function deleteData(req, res, next) {
         var id = req.params.id;
         rest.deleteDetail(name, id, res, next);
+    }
+
+    function _verifyUT(ut, res, cb) {
+        if (ut.user && ut.token) {
+            token.tokenValid(ut.user, ut.token, function(response) {
+                if (!response) {
+                    res.json({status: "fail", msg: "Your authentication failed. Log out and back in please. "+settings.errorPersistsMsg});
+                } else {
+                    cb();
+                }
+            });
+        }
     }
 
     return app;
--- modules/settings.js
+++ modules/settings.js
@@ -1,3 +1,4 @@
 module.exports = {
-    tokenExpireTime: 3600
+    tokenExpireTime: 3600,
+    errorPersistsMsg: "If the error persists, please contact the support at support@hushtag.co.uk"
 };
