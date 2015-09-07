d8510b7dcda60b3941e1976bac3389d32b9794ea
fgolemo <fgolemo@gmail.com>
2015-09-07 20:17:29 +0200

    upvoting/downvoting skeleton

--- modules/vote.js
+++ modules/vote.js
@@ -8,19 +8,57 @@ module.exports = (function () {
     var app = express.Router();
 
     app.post('/votes/hasVoted', hasVoted);
+    app.post('/votes/upvote', upvote);
+    app.post('/votes/downvote', downvote);
 
     function hasVoted(req, res, next) {
         var userToken = req.body.ut;
         var obj = req.body.obj;
+
+        _hasVoted(userToken, obj, res, function(response) {
+            res.json({status: "success", reply: response});
+        }, function(err) {
+            res.json({status: "fail", msg: "Currently can't process this request. "+settings.errorPersistsMsg});
+        });
+    }
+
+    function _hasVoted(userToken, obj, res, cb, cbErr) {
         token.verifyUT(userToken, res, function() {
             //console.log("INFO: posting obj: "+name+":"+id);
             var hash = obj.type+':'+obj.id+":voters";
             rest.client.sismemberAsync([hash, userToken.user]).then(function(response) {
-                res.json({status: "success", reply: response});
+                cb(response);
             }).error(function (err) {
                 console.log("ERROR: couldn't determine if user "+userToken.user+" has voted for "+obj.type+" "+obj.id);
-                res.json({status: "fail", msg: "Currently can't process this request. "+settings.errorPersistsMsg});
+                if (cbErr) {
+                    cbErr(err);
+                }
             });
+        });
+    }
+
+    function upvote(req, res, next) {
+        console.log("incomming upvote");
+        var userToken = req.body.ut;
+        var obj = req.body.obj;
+        _hasVoted(userToken, obj, res, function(response) {
+            console.log("upvoting, hv response");
+            console.dir(response);
+            res.json({});
+        }, function(err) {
+
+        });
+    }
+
+    function downvote(req, res, next) {
+        console.log("incomming downvote");
+        var userToken = req.body.ut;
+        var obj = req.body.obj;
+        _hasVoted(userToken, obj, res, function(response) {
+            console.log("downvoting, hv response");
+            console.dir(response);
+            res.json({});
+        }, function(err) {
 
         });
     }
